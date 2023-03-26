3657a333c527caf9020978b71d2685fc296a59f8
yaswant2403 <yse2@illiois.edu>
2023-03-25 23:46:46 -0500

    Cleaned up and added useful comment

--- correlation.py
+++ correlation.py
@@ -1,28 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
 
-
-# 1) Group Training Data month by month (month : dataframe of all statements from month)
-# 2) For each group, find correlation between charge off and variable
-    # a) If variable continuous, then use correlation analysis
-    # b) Categorical, use chi-square test
-
-# So, what we've been able to do so far is create a survival analysis model where we generate the percentage of bank statements that have
-# had a chargeoff based on credit score over the year of 2019. Now, we were thinking of using the percentage to help us predict the future
-# chargeoffs of only one month. So, how do we use something that's scaled over a year to predict the future of one month?
-# 1 month survival analysis but not possible
-
-# Step 1
-# Group Training Data by Month
+# Group Subset of Training Data by Month
 df = pd.read_csv("data/train_mini.csv")
 all_months = df['mth_code'].unique()
 month_statements = {}
 for month in all_months:
     month_statements[month] = df[df.mth_code == month].reset_index()
 
-# Replacing Credit Score Ranges
+# Replacing Credit Score Ranges with Median
 scores = [(581,600),(721,740),(761,850),(661,680),(601,620), (701,720), (621,640), (561,580), (641,660), (741,760), (681,700),(560,300)]
 medians = [(tup[0] + tup[1])/2 for tup in scores]
 ranges = { }
@@ -35,17 +22,19 @@ for mid in medians:
     else:
         key = str(fico_range[0]) + '-' + str(fico_range[1])
         ranges[key] = mid
+ranges['Exception'] = 0
+ranges['Missing'] = 0
 for month in all_months:
     month_statements[month].replace({"bank_fico_buckets_20": ranges}, inplace=True)
 
-# Numerical Variables
 
-# Make a dataframe containing only numerical variables and chargeoff column
+# All Numerical variables
 numerical_iv = ["bank_fico_buckets_20","mob","nbr_mths_due","variable_rate_margin","stmt_balance", "prev_balance","net_sales","net_payments",
                 "credit_limit_amt","credit_limit_pa", "principal_amt", "total_writeoff_amt","fee_chg_off_reversal_amt", "net_finance_charge", 
                 "non_principal_amount_gross","non_principal_amount_net", "non_principal_amount_stmt", "aged_writeoff_amt","bankruptcy_writeoff_amt", "fc_reversals", 
                 "fee_reversals","fraud_writeoff_amt","other_writeoff_amt", "promo_bal_amt","recovery_amt"] #iv (x)
-# charge_off is dv (y)
+
+# Creating Dictionary with Key = Month : Val = Statements with only IV columns and charge-off
 numerical_month_statements = {}
 for month in all_months:
     d = {}
@@ -53,7 +42,7 @@ for month in all_months:
         d[iv] = month_statements[month][iv]
     numerical_month_statements[month] = pd.DataFrame(data=d)
 
-# create correlation plot
+# Create Sample Correlation plots with specific independent variables
 x = numerical_month_statements[201910]['principal_amt']
 y = month_statements[201910]['charge_off']
 fig = plt.figure()
@@ -84,38 +73,55 @@ ax3.scatter(y3,x3,color="lightgreen")
 ax3.set_xlim(-0.5,1.5) # center the two tick marks in the middle
 ax3.set_xticks([0,1],labels=[r'$No\ Charge-Off$', r'$Charge-Off$'])
 ax3.set_xlabel(r'$\ Status\ of\ Charge-Off$')
-ax3.set_ylabel(r'$\ FICO\ Limit\ (\$) $')
+ax3.set_ylabel(r'$\ FICO\ Score\ $')
 ax3.set_title('10/2019 - Charge-Offs based on FICO Score')
 
-# for iv in numerical_iv:
-#     x = numerical_month_statements[month][iv]
-#     y = month_statements[month]['charge_off']
-#     fig = plt.figure()
-#     ax = fig.add_subplot(1,1,1)
-#     ax.scatter(y,x,color="lightblue")
-#     ax.set_xlim(-0.5,1.5) # center the two tick marks in the middle
-#     ax.set_xticks([0,1],labels=[r'$No\ Charge-Off$', r'$Charge-Off$'])
-#     ax.set_xlabel(r'$\ Status\ of\ Charge-Off$')
-#     ax.set_ylabel(r'$\ {iv}\ (\$) $')
-#     ax.set_title(f'Charge-Offs based on {iv}')
+month = np.random.choice(np.asarray(all_months)) # set a random month 
 
-# create correlation tests
+# To generate distinct colors for scatter plots
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+colors = get_cmap(len(numerical_iv))
 
+# Generating scatter plots for random months showcasing the 
+# correlation between the different independent numerical variables
+for iv in numerical_iv:
+    x = numerical_month_statements[month][iv]
+    y = month_statements[month]['charge_off']
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.scatter(y,x,c=colors(numerical_iv.index(iv)))
+    ax.set_xlim(-0.5,1.5) # center the two tick marks in the middle
+    ax.set_xticks([0,1],labels=[r'$No\ Charge-Off$', r'$Charge-Off$'])
+    ax.set_xlabel(r'$\ Status\ of\ Charge-Off$')
+    ax.set_ylabel(f'{iv}')
+    ax.set_title(f'{str(month)[4:] + "/" + str(month)[:-2]} Charge-Offs Based on {iv}')
 
+''''
 
-# Categorical Variables
-# #categorical = ["financial active", "net_payment_behaviour_tripd", "promotion_flag", "account_status_code", "ever_delinquent_flg", "active_12_mths",
-#                 "open_closed_flag", "purchase_active", "closed", "active", "charge_off", "charge_off_age", "charge_off_bk", "writeoff_type_bko", 
-#                 "writeoff_type_fraud_kiting", "writeoff_type_fraud_synthetic", "writeoff_type_deceased", "writeoff_type_other", "writeoff_type_aged",
-#                 "writeoff_type_settlement", "writeoff_type_fraud_other", "writeoff_type_repo", "writeoff_type_null", "industry", "writeoffdate", "chargeoffreasoncode", varrateidx]
-# Make a dataframe containing only categorical variables and chargeoff column
-# do df.corr()
-# create correlation plot
-# create correlation tests
+ANALYSIS OF CATEGORICAL VARIABLES
 
+'''
 
-# Step 2
-# 
+categorical = ["financial active", "net_payment_behaviour_tripd", "promotion_flag", "account_status_code", "ever_delinquent_flg", "active_12_mths",
+                "open_closed_flag", "purchase_active", "closed", "active", "charge_off", "charge_off_age", "charge_off_bk", "writeoff_type_bko", 
+                "writeoff_type_fraud_kiting", "writeoff_type_fraud_synthetic", "writeoff_type_deceased", "writeoff_type_other", "writeoff_type_aged",
+                "writeoff_type_settlement", "writeoff_type_fraud_other", "writeoff_type_repo", "writeoff_type_null", "industry", "writeoffdate", "chargeoffreasoncode", varrateidx]
+# Set up crosstab for categorical variables
+col_array = [month_statements[month]['financial_active'], month_statements[month]['net_payment_behaviour_tripd'], 
+            month_statements[month]['promotion_flag'], month_statements[month]['account_status_code'], 
+            month_statements[month]['ever_delinquent_flg'], month_statements[month]['active_12_mths'], month_statements[month]['open_closed_flag'],
+            month_statements[month]['purchase_active'], month_statements[month]['closed'], month_statements[month]['active'], month_statements[month]['charge_off'],
+            month_statements[month]['charge_off_aged'], month_statements[month]['charge_off_bk'], month_statements[month]['writeoff_type_bko'],
+            month_statements[month]['writeoff_type_fraud_kiting'], month_statements[month]['writeoff_type_fraud_synthetic'], 
+            month_statements[month]['writeoff_type_deceased'], month_statements[month]['writeoff_type_other'], month_statements[month]['writeoff_type_aged'],
+            month_statements[month]['writeoff_type_settlement'], month_statements[month]['writeoff_type_fraud_other'], month_statements[month]['writeoff_type_repo'],
+            month_statements[month]['writeoff_type_null'], month_statements[month]['industry']]
+crosstab = pd.crosstab(month_statements[month]['charge_off'],
+                    col_array, margins=True, margins_name="Total")
+
 
 
 
